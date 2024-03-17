d051440c4bc44755fefcf62cde8df20fc03c6ca0
Subham Mukhopadhyay <86921732+mukherjee-subham@users.noreply.github.com>
2024-03-17 23:45:41 +0530

    Add search bar component, search results page and navigation

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,89 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const formSchema = z.object({
+  searchQuery: z
+    .string({
+      required_error: "Restaurant name is required",
+    })
+    .min(1, "City is required"),
+});
+
+export type SearchForm = z.infer<typeof formSchema>;
+
+type Props = {
+  onSubmit: (formData: SearchForm) => void;
+  onReset?: () => void;
+  placeholder: string;
+};
+
+const SearchBar = ({ onSubmit, onReset, placeholder }: Props) => {
+  const form = useForm<SearchForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      searchQuery: "",
+    },
+  });
+
+  const handleReset = () => {
+    form.reset({
+      searchQuery: "",
+    });
+
+    if (onReset) {
+      onReset();
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={`flex items-center justify-between flex-1 flex-row gap-3 border-2 rounded-full p-3 mx-5 ${
+          form.formState.errors.searchQuery && "border-red-500"
+        }`}
+      >
+        <Search
+          strokeWidth={2.5}
+          size={30}
+          className="ml-1 text-orange-500 hidden md:block"
+        />
+        <FormField
+          control={form.control}
+          name="searchQuery"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormControl>
+                <Input
+                  {...field}
+                  className="border-none shadow-none text-xl focus-visible:ring-0"
+                  placeholder={placeholder}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        {form.formState.isDirty && (
+          <Button
+            type="button"
+            variant={"outline"}
+            className="rounded-full"
+            onClick={handleReset}
+          >
+            Clear
+          </Button>
+        )}
+        <Button type="submit" className="rounded-full bg-orange-500">
+          Search
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SearchBar;
