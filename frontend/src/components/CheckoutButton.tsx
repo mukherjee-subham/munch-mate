41842d46dc5b508def7d9cefd0d4f8b9696b0dd3
Subham Mukhopadhyay <86921732+mukherjee-subham@users.noreply.github.com>
2024-03-28 04:11:38 +0530

    Add confirm address modal tocheckout button

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -2,8 +2,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 import LoadingButton from "./LoadingButton";
 import { useLocation } from "react-router-dom";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import UserProfileForm, {
+  UserFormData,
+} from "@/forms/user-profile-form/UserProfileForm";
+import { useGetMyUser } from "@/api/MyUserApi";
 
-const CheckoutButton = () => {
+type Props = {
+  onCheckout: (userFormData: UserFormData) => void;
+  disabled: boolean;
+};
+
+const CheckoutButton = ({ onCheckout, disabled }: Props) => {
   const {
     isAuthenticated,
     isLoading: isAuthLoading,
@@ -11,6 +21,8 @@ const CheckoutButton = () => {
   } = useAuth0();
 
   const { pathname } = useLocation();
+
+  const { userData, isLoading: isGetUserLoading } = useGetMyUser();
 
   const onLogin = async () => {
     loginWithRedirect({
@@ -31,14 +43,32 @@ const CheckoutButton = () => {
     );
   }
 
-  if (isAuthLoading) {
+  if (isAuthLoading || !userData) {
     return <LoadingButton />;
   }
 
   return (
-    <Button className="font-bold bg-orange-500 border-white border-2 hover:text-orange-500 hover:bg-white hover:border-orange-500 hover:border-solid hover:border-2 flex-1">
-      Checkout!
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          disabled={disabled}
+          className="font-bold bg-orange-500 border-white border-2 hover:text-orange-500 hover:bg-white hover:border-orange-500 hover:border-solid hover:border-2 flex-1"
+        >
+          Checkout!
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[425px] md:min-w-[700px] bg-gray-50">
+        <UserProfileForm
+          userData={userData}
+          onSave={() => {
+            onCheckout(userData);
+          }}
+          isLoading={isGetUserLoading}
+          buttonText="Checkout!"
+          title="Confirm delivery details"
+        />
+      </DialogContent>
+    </Dialog>
   );
 };
 
